d6d0e650cb161e131644a9dd31dde9fdabcd4585
Taroro28 <tarotaro0208@gmail.com>
2019-11-10 00:58:33 +0900

    コメントを追加

--- auto_tweet.py
+++ auto_tweet.py
@@ -18,17 +18,21 @@ my_screen_name = my_api.screen_name
 
 def job():
     # 自分のタイムライン
+    # tweet_mode='extended'で最大文字数を抽出
     results = api.user_timeline(screen_name= my_screen_name, count=100, tweet_mode='extended')
 
+    # ツイート内容を格納
     tweet_list = []
-
     for result in results:
+        # リツイート、リプライ、引用を除外
         if not '@' in result.full_text:
             tweet_list.append(result.full_text)
 
+    # 最新のツイート内容を代入
     latest_tweet = re.sub(r'https.+$', '', tweet_list.pop(0))
 
     for index, tweet in enumerate(tweet_list):
+        # 過去に同じツイートをした日の翌日の内容をツイートする
         if re.sub(r'https.+$', '', tweet) == latest_tweet:
             api.update_status(tweet_list[index-1])
             print("ツイートしました")
