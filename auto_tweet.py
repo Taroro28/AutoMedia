5231e59ee523f776c0d336b380d142a978eaaa3c
Taroro28 <tarotaro0208@gmail.com>
2019-11-12 13:25:19 +0900

    add comment

--- auto_tweet.py
+++ auto_tweet.py
@@ -2,7 +2,7 @@
 import tweepy, config, time, schedule, re
 import datetime as dt
 
-# TwitterAPI
+# Twitter API
 CK = config.CONSUMER_KEY
 CS = config.CONSUMER_SECRET
 AT = config.ACCESS_TOKEN
@@ -17,32 +17,36 @@ my_id = my_api.id
 my_screen_name = my_api.screen_name
 
 def job():
-    # 自分のタイムライン
-    # tweet_mode='extended'で最大文字数を抽出
-    results = api.user_timeline(screen_name= my_screen_name, count=100, tweet_mode='extended')
 
-    # ツイート内容を格納
+    # my timeline
+    # tweet_mode='extended' for get full text
+    # count is amount of tweets
+    my_timeline = api.user_timeline(screen_name= my_screen_name, count=100, tweet_mode='extended')
+
+    # tweets without retweet, reply and quote
     tweet_list = []
-    for result in results:
-        # リツイート、リプライ、引用を除外
-        if not '@' in result.full_text:
-            tweet_list.append(result.full_text)
+    for tweet in my_timeline:
+        if not '@' in tweet.full_text:
+            tweet_list.append(tweet.full_text)
 
-    # 最新のツイート内容を代入
+    # latest tweet which text has URL
     latest_tweet = re.sub(r'https.+$', '', tweet_list.pop(0))
 
+    # tweet the text of next day of the day when I tweeted the same text before.
     for index, tweet in enumerate(tweet_list):
-        # 過去に同じツイートをした日の翌日の内容をツイートする
+        # same as latest tweet
         if re.sub(r'https.+$', '', tweet) == latest_tweet:
+            # tweet tweet of next day of that day
             api.update_status(tweet_list[index-1])
-            print("ツイートしました")
-            print(dt.datetime.now())
+
+            # validation on terminal
+            print("No Problem!", dt.datetime.now())
             break
 
-# 毎日15:00に実行
+# run every 15:00
 schedule.every().day.at("15:00").do(job)
 
+# check the task every minute
 while True:
     schedule.run_pending()
-    # スリープごとに確認
     time.sleep(60)
